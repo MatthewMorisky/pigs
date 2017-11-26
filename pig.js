ea565783ad4507d0dcc24c0c5d509ee3d21b7718
Matthew Morisky <matthew.morisky@gmail.com>
2017-11-26 01:45:02 -0800

    Added bushes and attacking animations

--- pig.js
+++ pig.js
@@ -3,7 +3,11 @@ function Pig() {
 	this.radius = 20;
 	this.heading = 0;
 	this.velocity = createVector(0,0);
-
+	this.hasWeapon = false;
+	this.weaponCount = 0;
+	this.weaponAngle = 0;
+	this.isAttacking = false;
+	this.isRecovering = false;
 	
 
 
@@ -12,6 +16,7 @@ function Pig() {
 		this.update();
 		this.show();
 		this.move();
+		if (this.isAttacking) this.attack();
 		
 		
 	}
@@ -19,16 +24,44 @@ function Pig() {
 		if (keyIsDown(UP_ARROW)) {
 			this.thrust();
 		}
+		else if (keyIsDown(DOWN_ARROW)) {
+			this.backThrust();
+		}
+
 		if ( (keyIsDown(LEFT_ARROW) || keyIsDown(RIGHT_ARROW) ) && !( (keyIsDown(LEFT_ARROW) && keyIsDown(RIGHT_ARROW) ) ) ) {
 			if (keyIsDown(LEFT_ARROW)) {
-				this.turn(-.1);
+				this.turn(-.07);
 			}
 			else if (keyIsDown(RIGHT_ARROW)) {
-				this.turn(.1);
+				this.turn(.07);
 			}
 		}
 		else {
 			this.turn(0);
+		}
+	}
+	this.attack = function() {
+		if (this.hasWeapon) {
+			this.weaponCount = this.weaponCount + .4
+			if (this.weaponCount > 12.8) {
+				this.isAttacking = false;
+				this.weaponCount = 0;
+			}
+			this.weaponAngle = -PI/7 * Math.sin(this.weaponCount);
+
+		}
+		else if(!this.isRecovering) {
+			var force = p5.Vector.fromAngle(this.heading);
+			force.mult(20);
+			this.velocity.add(force);
+			this.isRecovering = true;
+		}
+		else if(this.isRecovering) {
+			var force = p5.Vector.fromAngle(this.heading);
+			force.mult(-20);
+			this.velocity.add(force);
+			this.isRecovering = false;
+			this.isAttacking = false;
 		}
 	}
 
@@ -46,7 +79,14 @@ function Pig() {
 	this.thrust = function() {
 
 		var force = p5.Vector.fromAngle(this.heading);
-		force.mult(0.4);
+		force.mult(.4);
+		this.velocity.add(force);
+		
+	}
+	this.backThrust = function() {
+
+		var force = p5.Vector.fromAngle(this.heading);
+		force.mult(-0.13);
 		this.velocity.add(force);
 		
 	}
@@ -56,6 +96,11 @@ function Pig() {
 		translate(this.pos.x, this.pos.y);
 		rotate(this.heading + PI/2);
 		triangle(-this.radius, this.radius, this.radius, this.radius, 0, -this.radius);
+		if (this.hasWeapon) {
+			fill(178, 110, 16);
+			rotate(this.weaponAngle);
+			rect(-25, -5, 10, -80);
+		}
 		pop();
 
 
--- sketch.js
+++ sketch.js
@@ -1,22 +1,28 @@
 
-
 function setup() {
 
 	createCanvas(500, 500); 	
 	pig = new Pig();
-
+	bush = new Bush();
+	//frameRate(1);
 
 }
 
 function draw() {
-	background(21, 173, 1);
+	background(60, 163, 92);
+	bush.exe();
 	pig.exe();
+
 
 		
 }
 function keyPressed() {
 	if (keyCode === 32) { //Spacebar
+		pig.isAttacking = true;
 		}
+	if (keyCode === 49) {
+		pig.hasWeapon = !pig.hasWeapon;
+	}
 }
 
 function keyReleased() {
