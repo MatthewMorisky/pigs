1ba74fdc21661b2644e80429df3f88c6add7bb35
Matthew Morisky <matthew.morisky@gmail.com>
2017-12-04 19:40:47 -0800

    Moving bushes

--- src/bush.js
+++ src/bush.js
@@ -32,7 +32,7 @@ function Bush(x, y) {
 	this.show = function() {
 		
 		noFill();
-		image(this.img, -this.radius*2, -this.radius);
+		image(this.img, this.x -this.radius*2, this.y -this.radius);
 		ellipse(this.x, this.y, this.radius*2, this.radius);
 		
 	}
